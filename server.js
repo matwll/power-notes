3d348865a9c38d047f10e72f436c968b8642819b
Matthew Williams <maw01091989@gmail.com>
2022-12-11 20:52:10 -0800

    worked on the get and post res for /api/notes

--- server.js
+++ server.js
@@ -7,14 +7,35 @@ const generateUuid = uuid.v4();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html')) 
+    res.sendFile(path.join(__dirname, './public/notes.html')) 
 });
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, notes) => {
+        if (err){
+            console.log(err);
+        }else{
+            const savedNotes = JSON.parse(notes);
+        }
+    })
+})
 
-app.listen(3000);
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, notes) => {
+        if (err){
+            console.log(err);
+        }else{
+            const savedNotes = JSON.parse(notes);
+        }
+    })
+    fs.writeFile('./db/db.json', (err) => {
+        
+    })
+})
+
+app.listen(PORT);

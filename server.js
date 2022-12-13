3c974432d907454e4e17241d98fd432d3937e243
Matthew Williams <maw01091989@gmail.com>
2022-12-12 18:11:08 -0800

    created the notes/post handler

--- server.js
+++ server.js
@@ -2,40 +2,55 @@ const uuid = require("uuid");
 const express = require("express");
 const path = require('path');
 const fs = require('fs');
+const { json } = require("express");
 
 const generateUuid = uuid.v4();
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html')) 
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, notes) => {
-        if (err){
-            console.log(err);
-        }else{
-            const savedNotes = JSON.parse(notes);
-        }
-    })
+    res.status(200).json(reviews);
 })
 
 app.post('/api/notes', (req, res) => {
+    console.log(req);
+
+    const { title, text } = req.body;
+
+    if (title && text){
+        const newNote = {
+            title,
+            text,
+            noteId: generateUuid(),
+        }
+    }
+
     fs.readFile('./db/db.json', (err, notes) => {
         if (err){
             console.log(err);
         }else{
-            const savedNotes = JSON.parse(notes);
+            let savedNotes = JSON.parse(notes);
+
+            savedNotes.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(savedNotes, null, 4));
         }
     })
-    fs.writeFile('./db/db.json', (err) => {
-        
-    })
+    
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+
+    res.json(response)
+    console.log(res);
 })
 
 app.listen(PORT);
